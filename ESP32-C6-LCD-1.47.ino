43224c3ff0b03021e0a7dd84ef591426a198676c
charles <cgiguere@iname.com>
2025-05-05 19:53:03 -0400

    playSelectedGif

--- ESP32-C6-LCD-1.47.ino
+++ ESP32-C6-LCD-1.47.ino
@@ -16,8 +16,8 @@ AnimatedGIF gif;
 #define MAX_FILES 20 // Adjust as needed
 String gifFileList[MAX_FILES];
 uint32_t gifFileSizes[MAX_FILES] = {0}; // Store each GIF file's size in bytes
-int fileCount = 0;
-static int currentFile = 0;
+int gifCount = 0;
+static int currentGifIndex = 0;
 static File FSGifFile; // temp gif file holder
 
 void setup()
@@ -28,7 +28,8 @@ void setup()
   LCD_Init();
   SD_Init();
   loadGifFilesList();
-  pinMode(BOOT_KEY_PIN, INPUT);   
+  pinMode(BOOT_KEY_PIN, INPUT);
+  gif.begin(BIG_ENDIAN_PIXELS);
 
   // tft.setTextColor(TFT_GREEN);
   // tft.setFont(FONT_12x16);
@@ -38,7 +39,194 @@ void setup()
 
 void loop()
 {
-  delay(1);
+  playSelectedGif(currentGifIndex);
+}
+
+// Callback function to open a gif file from the SD card
+static void *GIFOpenFile(const char *fname, int32_t *pSize)
+{
+  Serial.printf("Opening %s from SD\n", fname);
+  FSGifFile = SD.open(fname);
+  if (FSGifFile)
+  {
+    *pSize = FSGifFile.size();
+    return (void *)&FSGifFile;
+  }
+  return NULL;
+}
+
+// Callback function to close a gif file from the SD card
+static void GIFCloseFile(void *pHandle)
+{
+  File *f = static_cast<File *>(pHandle);
+  if (f != NULL)
+    f->close();
+}
+
+// Callback function to read a gif file from the SD card
+static int32_t GIFReadFile(GIFFILE *pFile, uint8_t *pBuf, int32_t iLen)
+{
+  int32_t iBytesRead;
+  iBytesRead = iLen;
+  File *f = static_cast<File *>(pFile->fHandle);
+  // Note: If you read a file all the way to the last byte, seek() stops working
+  if ((pFile->iSize - pFile->iPos) < iLen)
+    iBytesRead = pFile->iSize - pFile->iPos - 1; // <-- ugly work-around
+  if (iBytesRead <= 0)
+    return 0;
+  iBytesRead = (int32_t)f->read(pBuf, iBytesRead);
+  pFile->iPos = f->position();
+  return iBytesRead;
+}
+
+// Callback function to seek a gif file from the SD card
+static int32_t GIFSeekFile(GIFFILE *pFile, int32_t iPosition)
+{
+  int i = micros();
+  File *f = static_cast<File *>(pFile->fHandle);
+  f->seek(iPosition);
+  pFile->iPos = (int32_t)f->position();
+  i = micros() - i;
+  // log_d("Seek time = %d us\n", i);
+  return pFile->iPos;
+}
+
+// Callback function to Draw a line of image directly on the screen
+void GIFDraw(GIFDRAW *pDraw)
+{
+  uint8_t *s;
+  uint16_t *d, *usPalette, usTemp[LCD_WIDTH];
+  int x, y, iWidth;
+
+  iWidth = pDraw->iWidth;
+  if (iWidth + pDraw->iX > LCD_WIDTH)
+  {
+    iWidth = LCD_WIDTH - pDraw->iX;
+  }
+  usPalette = pDraw->pPalette;
+  y = pDraw->iY + pDraw->y; // current line
+  if (y >= LCD_HEIGHT || pDraw->iX >= LCD_WIDTH || iWidth < 1)
+  {
+    return;
+  }
+  s = pDraw->pPixels;
+  if (pDraw->ucDisposalMethod == 2) // restore to background color
+  {
+    for (x = 0; x < iWidth; x++)
+    {
+      if (s[x] == pDraw->ucTransparent)
+      {
+        s[x] = pDraw->ucBackground;
+      }
+    }
+    pDraw->ucHasTransparency = 0;
+  }
+
+  // Apply the new pixels to the main image
+  if (pDraw->ucHasTransparency) // if transparency used
+  {
+    uint8_t *pEnd, c, ucTransparent = pDraw->ucTransparent;
+    int x, iCount;
+    pEnd = s + iWidth;
+    x = 0;
+    iCount = 0; // count non-transparent pixels
+    while (x < iWidth)
+    {
+      c = ucTransparent - 1;
+      d = usTemp;
+      while (c != ucTransparent && s < pEnd)
+      {
+        c = *s++;
+        if (c == ucTransparent) // done, stop
+        {
+          s--; // back up to treat it like transparent
+        }
+        else // opaque
+        {
+          *d++ = usPalette[c];
+          iCount++;
+        }
+      } // while looking for opaque pixels
+      if (iCount) // any opaque pixels?
+      {
+        LCD_addWindow(
+            pDraw->iX + x,              // Xstart
+            y,                          // Ystart
+            pDraw->iX + x + iCount - 1, // Xend  (inclusive)
+            y,                          // Yend   (only one scan‑line tall)
+            usTemp                      // pixel buffer (RGB565, big‑endian)
+        );
+        x += iCount;
+        iCount = 0;
+      }
+      // no, look for a run of transparent pixels
+      c = ucTransparent;
+      while (c == ucTransparent && s < pEnd)
+      {
+        c = *s++;
+        if (c == ucTransparent)
+        {
+          iCount++;
+        }
+        else
+        {
+          s--;
+        }
+      }
+      if (iCount)
+      {
+        x += iCount; // skip these
+        iCount = 0;
+      }
+    }
+  }
+  else
+  {
+    s = pDraw->pPixels;
+    // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
+    for (x = 0; x < iWidth; x++)
+    {
+      usTemp[x] = usPalette[*s++];
+    }
+    LCD_addWindow(
+        pDraw->iX,              // Xstart
+        y,                      // Ystart
+        pDraw->iX + iWidth - 1, // Xend
+        y,                      // Yend
+        usTemp                  // pixel buffer
+    );
+  }
+}
+
+// Play a gif directly from the SD card
+void gifPlayFromSDCard(char *gifPath)
+{
+
+  if (!gif.open(gifPath, GIFOpenFile, GIFCloseFile, GIFReadFile, GIFSeekFile, GIFDraw))
+  {
+    Serial.printf("Could not open gif %s", gifPath);
+  }
+  else
+  {
+    Serial.printf("Starting playing gif %s\n", gifPath);
+
+    while (gif.playFrame(false /*change to true to use the internal gif frame duration*/, NULL))
+    {
+    }
+
+    gif.close();
+  }
+}
+
+void playSelectedGif(int gifIndex)
+{
+  // Build the full path for the selected GIF.
+  String fullPath = String(GIF_FOLDER) + "/" + gifFileList[gifIndex];
+  char gifFilename[128];
+  fullPath.toCharArray(gifFilename, sizeof(gifFilename));
+
+  Serial.printf("Playing %s\n", gifFilename);
+  gifPlayFromSDCard(gifFilename);
 }
 
 void SD_Init()
@@ -95,7 +283,7 @@ void loadGifFilesList()
     Serial.println("Failed to open GIF folder");
     return;
   }
-  fileCount = 0;
+  gifCount = 0;
   while (true)
   {
     File file = gifDir.openNextFile();
@@ -106,19 +294,19 @@ void loadGifFilesList()
       String name = file.name();
       if (name.endsWith(".gif") || name.endsWith(".GIF"))
       {
-        gifFileList[fileCount] = name;
-        gifFileSizes[fileCount] = file.size(); // Save file size (in bytes)
-        fileCount++;
-        if (fileCount >= MAX_FILES)
+        gifFileList[gifCount] = name;
+        gifFileSizes[gifCount] = file.size(); // Save file size (in bytes)
+        gifCount++;
+        if (gifCount >= MAX_FILES)
           break;
       }
     }
     file.close();
   }
   gifDir.close();
-  Serial.printf("%d gif files read\n", fileCount);
+  Serial.printf("%d gif files read\n", gifCount);
   // Optionally, print out each file's size for debugging:
-  for (int i = 0; i < fileCount; i++)
+  for (int i = 0; i < gifCount; i++)
   {
     Serial.printf("File %d: %s, Size: %lu bytes\n", i, gifFileList[i].c_str(), gifFileSizes[i]);
   }
